85b13de811ee4a2c6cdb0f5dc494215bc659caa7
Vincenzo Ciancia <vincenzoml@gmail.com>
2020-02-01 15:02:32 +0100

    WIP

--- src/SITKUtil.fs
+++ src/SITKUtil.fs
@@ -222,13 +222,13 @@ type VoxImage private (img : Image,uniqueName : string) =
     //let threadid = System.Threading.Thread.CurrentThread       
     let hashImg = img.GetHashCode().ToString() // TODO: hash the image data (voxels and headers) instead of using .NET's hashing (what's that based on, in this case, anyway?)
     let disposed = ref false
-    static let internalNum = ref 0
-    static let internalId () = 
-        lock internalNum 
-            (fun () -> 
-                let res = !internalNum
-                internalNum := res + 1
-                res)
+    // static let internalNum = ref 0
+    // static let internalId () = 
+    //     lock internalNum 
+    //         (fun () -> 
+    //             let res = !internalNum
+    //             internalNum := res + 1
+    //             res)
     let dispose () =
         lock disposed (fun () -> if not !disposed then disposed := true; img.Dispose())
 
@@ -251,7 +251,7 @@ type VoxImage private (img : Image,uniqueName : string) =
     override __.ToString() = sprintf "{ hash: %s; uniqueName : %s }" hashImg uniqueName
 
     new (img : Image) =
-        new VoxImage(img,sprintf "internal:%d" (internalId()))
+        new VoxImage(img,"internal") // sprintf "internal:%d" (internalId()))
 
     new (filename : string) = 
         // WARNING: the program assumes this function always returns a float32 image. Be cautious before changing this.
