1398371d2382bf080edc775536b2b9e672b69fe5
Vincenzo Ciancia <vincenzoml@gmail.com>
2020-07-24 13:43:51 +0200

    main program to slice brats dataset to 2d; use Runme.sh

--- src/Program.fs
+++ src/Program.fs
@@ -17,6 +17,10 @@
 module VoxLogicA.Main
 open System.Reflection
 open Argu
+open itk.simple
+open Microsoft.FSharp.NativeInterop
+open System.IO
+
 
 exception CommandLineException 
     with override __.Message = "Invalid arguments. Usage:\nVoxLogicA <FILENAME>"
@@ -27,17 +31,51 @@ type LoadFlags = {
 }
 
 type CmdLine = 
-    | Ops 
-    | Sequential
+    | Ext of string
     | [<MainCommandAttribute;UniqueAttribute>] Filename of string    
 with
     interface Argu.IArgParserTemplate with
         member s.Usage =
             match s with
-            | Ops ->  "display a list of all the internal operators, with their types and a brief description"
-            | Filename _ -> "VoxLogicA session file"
-            | Sequential ->  "Run on one CPU only"
-    
+            | Filename _ -> "BRATS-style directory"
+            | Ext _ -> "Extension of the file to slice"
+
+let getSize (slice : Image) =
+    let slice = SimpleITK.Cast(slice,PixelIDValueEnum.sitkInt32)
+    let mutable count = 0
+    let obj = System.Object()
+    let niBuffer = slice.GetBufferAsInt32 ()
+    use buffer = new NativeArray<int32>(NativePtr.ofNativeInt niBuffer,(int <| slice.GetNumberOfPixels()) * (int <| slice.GetNumberOfComponentsPerPixel()),obj)
+    for i = 0 to buffer.Length - 1 do
+        if buffer.UGet i > 0 then 
+            count <- count + 1
+    count        
+
+let slice dirname (flairF : string) (segF : string) =
+    let flair = SimpleITK.ReadImage flairF
+    let seg = SimpleITK.ReadImage segF    
+    let dimsInt = seg.GetSize()
+    let dims = dimsInt |> Seq.map int |> Seq.toArray
+    let sizes = Array.create dims.[2] 0
+    let extractSize = new VectorUInt32(dimsInt)
+    extractSize.[2] <- 0u
+    let extractIdx = new VectorInt32([|0;0;0|])
+    extractIdx.[0] <- 0
+    extractIdx.[1] <- 0
+    for i = 0 to dims.[2] - 1 do
+        extractIdx.[2] <- int32 i
+        use slice = SimpleITK.Extract(seg,extractSize,extractIdx)
+        sizes.[i] <- getSize slice
+    let candidate = Array.findIndex (fun el -> el = Array.max sizes) sizes
+    extractIdx.[2] <- candidate
+    let result = SimpleITK.Cast(SimpleITK.Extract(flair,extractSize,extractIdx),PixelIDValueEnum.sitkUInt16)
+    let resultSeg = SimpleITK.Cast(SimpleITK.Extract(seg,extractSize,extractIdx),PixelIDValueEnum.sitkUInt16)
+    let basename = flairF.Substring(0,flairF.LastIndexOf ".nii.gz") + ""
+    let fnameUint16png =  basename + "_slice-max-GTV-uint16.png"
+    let fnameSegPng = basename + "_seg-uint16.png"
+    SimpleITK.WriteImage (result,fnameUint16png)    
+    SimpleITK.WriteImage (resultSeg,fnameSegPng)  
+
 [<EntryPoint>]
 let main (argv : string array) =
     let name = Assembly.GetEntryAssembly().GetName()
@@ -50,29 +88,29 @@ let main (argv : string array) =
     try
         let cmdLineParser = ArgumentParser.Create<CmdLine>(programName = name.Name, errorHandler = ProcessExiter())     
         let parsed = cmdLineParser.Parse argv  
-        
-        if parsed.Contains Ops
-        then 
-            Seq.iter (fun (op : Operator) -> printfn "%s" <| op.Show()) checker.OperatorFactory.Operators
-            exit 0
-        let sequential = parsed.Contains Sequential        
-        // if sequential
-        // then 
-        //     let proc = System.Diagnostics.Process.GetCurrentProcess()
-        //     proc.ProcessorAffinity <- nativeint 0x1          
-        let run filename =
-            let interpreter = Interpreter(model,checker)
-            interpreter.Batch sequential interpreter.DefaultLibDir filename    
-        match (parsed.TryGetResult Filename,ErrorMsg.isDebug()) with 
-            | None,false ->                                      
-                printfn "%s\n" (cmdLineParser.PrintUsage ())
-                0
-            | Some filename,_ -> 
-                run filename        
-                0
-            | None,true ->
-                run "test.imgql"
-                0
+           
+        let filename = 
+            match parsed.TryGetResult Filename with 
+            | Some f -> f 
+            | None -> "../BRATS"
+
+        let ext = 
+            match parsed.TryGetResult Ext with 
+            | Some e -> e 
+            | None -> "pflair-uint16-alike.nii.gz"
+
+        let d = System.IO.Directory.GetDirectories filename
+        for directory in d do
+            let files = System.IO.Directory.GetFiles directory
+            let flair = Array.tryFind (fun (fname : string) -> fname.EndsWith ext) files
+            let seg = Array.tryFind (fun (fname : string) -> fname.EndsWith "seg.nii.gz") files
+            match (flair,seg) with
+            | (Some flairF,Some segF) -> 
+                ErrorMsg.Logger.Debug <| sprintf "slicing in directory %s, files %s and %s" directory flairF segF 
+                slice directory flairF segF
+            | _ -> ()
+        0
+
     with e ->        
             ErrorMsg.Logger.DebugExn e
             ErrorMsg.Logger.Failure "exiting."
