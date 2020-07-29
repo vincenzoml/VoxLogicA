d458123c1e4ce839d41da53dbfe41101b7605ad1
Vincenzo Ciancia <vincenzoml@gmail.com>
2020-07-29 12:12:14 +0200

    minor fixes

--- Runme.sh
+++ Runme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo checking command line arguments...
+echo checking command line arguments '(use "Runme.sh BRATS-LIKE-DIR")...'
 
 test -d "$1" || exit 1
 
@@ -8,9 +8,9 @@ echo Working on dataset "$1"
 
 ./examples/High-grade-Glioblastoma_BRATS/run_analysis.sh "$1" PrepareSliceExtraction.imgql
 (cd src && make && ./bin/release/netcoreapp3.1/linux-x64/VoxLogicA ../BRATS)
-find -L "$1" -name "*alike*" -print0 |
+find -L "$1" -name "*alike*" -print0 | 
 while read -d $'\0' FILE
 do OUTFILE=$(dirname $FILE)/$(basename $FILE .png)-scaled.png
-    convert $FILE -scale 3200% $OUTFILE
+    convert $FILE -scale 800% $OUTFILE
 done
-./examples/High-grade-Glioblastoma_BRATS/run_analysis.sh BRATS GBM-HGG-TACAS19-modified-for-uint16.imgql
+#./examples/High-grade-Glioblastoma_BRATS/run_analysis.sh BRATS GBM-HGG-TACAS19-modified-for-uint16.imgql
