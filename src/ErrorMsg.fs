7f736ff38db074f53edc338584fac2d5dcc8abc7
Vincenzo Ciancia <vincenzoml@gmail.com>
2020-12-14 13:43:05 +0100

    WIP

--- src/ErrorMsg.fs
+++ src/ErrorMsg.fs
@@ -14,39 +14,59 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-module VoxLogicA.ErrorMsg 
+module VoxLogicA.ErrorMsg
+
 open System.IO
-// TODO: convert into a new type called Logger 
+// TODO: convert into a new type called Logger
 
 let mutable private debugFlag = false
 #if DEBUG
 do debugFlag <- true
 #endif
-let isDebug() = debugFlag
+let isDebug () = debugFlag
 
-type Logger private () =        
-        static let streamWriter = new StreamWriter(System.Console.OpenStandardOutput())
-        static let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-        static let mutable destinations = [streamWriter] // this is a placeholder for permitting more simultaneous destinations.         
-        
-        static let print prefix (string : string) =            
-            let printer destination = 
-                lock destination 
-                    (fun () ->
-                        fprintfn destination "[%10dms] [%s] %s" 
-                            stopWatch.ElapsedMilliseconds 
-                            prefix 
-                            (string.Replace("\n","\n                      "))
-                        destination.Flush())
-            List.iter printer destinations               
+type Logger private () =
+    static let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    static let mutable destinations = [] // this is a placeholder for permitting more simultaneous destinations.
 
-        // private new() = Logger(new StreamWriter(System.Console.OpenStandardOutput()))
-        // private new(filename : string) = Logger(new StreamWriter(filename))
+    static let print prefix (string: string) =
+        let printer destination =
+            lock destination (fun () ->
+                fprintfn
+                    destination
+                    "[%10dms] [%s] %s"
+                    stopWatch.ElapsedMilliseconds
+                    prefix
+                    (string.Replace("\n", "\n                      "))
+                destination.Flush())
 
-        static member Debug s =  print "info" s
-        static member DebugOnly s = if isDebug() then print "dbug" s
-        static member Warning s = print "warn" s
-        static member Failure s = print "fail" s
-        static member Result name value = print "user" (sprintf "%s=%A" name value)
-        static member DebugExn (exn : exn) = Logger.Debug <| if isDebug() then exn.ToString() else exn.Message
-        
+        lock destinations (fun () -> List.iter printer destinations)
+
+    static member LogToStdout() =
+        lock destinations (fun () ->
+            let str = System.Console.OpenStandardOutput()
+            let sw = new StreamWriter(str)
+            destinations <- sw :: destinations)
+
+    static member LogToMemory() =
+        lock destinations (fun () ->
+            let str = new MemoryStream(1000000)
+            let sw = new StreamWriter(str)
+            let sr = new StreamReader(str)
+            destinations <- sw :: destinations
+            fun () -> 
+                sw.Flush()
+                ignore <| str.Seek(0L,SeekOrigin.Begin)
+                sr.ReadToEnd())
+
+    static member Debug s = print "info" s
+    static member DebugOnly s = if isDebug () then print "dbug" s
+    static member Warning s = print "warn" s
+    static member Failure s = print "fail" s
+
+    static member Result name value =
+        print "user" (sprintf "%s=%A" name value)
+
+    static member DebugExn(exn: exn) =
+        Logger.Debug
+        <| if isDebug () then exn.ToString() else exn.Message
--- src/Program.fs
+++ src/Program.fs
@@ -15,62 +15,84 @@
 // limitations under the License.
 
 module VoxLogicA.Main
+
 open System.Reflection
 open Argu
 
-exception CommandLineException 
-    with override __.Message = "Invalid arguments. Usage:\nVoxLogicA <FILENAME>"
+exception CommandLineException with
+    override __.Message =
+        "Invalid arguments. Usage:\nVoxLogicA <FILENAME>"
 
-type LoadFlags = {
-    fname : string;
-    numCores : int
-}
+type LoadFlags = { fname: string; numCores: int }
 
-type CmdLine = 
-    | Ops 
-    | [<MainCommandAttribute;UniqueAttribute>] Filename of string    
-with
+type CmdLine =
+    | [<UniqueAttribute>] Ops
+    | [<UniqueAttribute>] JSon
+    | [<MainCommandAttribute; UniqueAttribute>] Filename of string
     interface Argu.IArgParserTemplate with
         member s.Usage =
             match s with
-            | Ops ->  "display a list of all the internal operators, with their types and a brief description"
+            | Ops -> "display a list of all the internal operators, with their types and a brief description"
+            | JSon _ ->
+                "saves auxiliary information on saved layers, printed values, and the log file to a structured json format instead than on standard output"
             | Filename _ -> "VoxLogicA session file"
-    
+
+type JSonOut = FSharp.Data.JsonProvider<"example.json">
+
 [<EntryPoint>]
-let main (argv : string array) =
+let main (argv: string array) =
     let name = Assembly.GetEntryAssembly().GetName()
-    let version = name.Version 
-    let informationalVersion = ((Assembly.GetEntryAssembly().GetCustomAttributes(typeof<AssemblyInformationalVersionAttribute>, false).[0]) :?> AssemblyInformationalVersionAttribute).InformationalVersion
-    ErrorMsg.Logger.Debug (sprintf "%s %s" name.Name informationalVersion)
-    let model = SITKModel() :> IModel   
-    let checker = ModelChecker(model)          
-    if version.Revision <> 0 then ErrorMsg.Logger.Warning (sprintf "You are using a PRERELEASE version of %s. The most recent stable release is %d.%d.%d." name.Name version.Major version.Minor version.Build)                        
+    let version = name.Version
+
+    let informationalVersion =
+        ((Assembly.GetEntryAssembly().GetCustomAttributes(typeof<AssemblyInformationalVersionAttribute>, false).[0]) :?> AssemblyInformationalVersionAttribute).InformationalVersion
+
+    ErrorMsg.Logger.Debug(sprintf "%s %s" name.Name informationalVersion)
+    let model = SITKModel() :> IModel
+    let checker = ModelChecker model
+    if version.Revision <> 0 then
+        ErrorMsg.Logger.Warning
+            (sprintf
+                "You are using a PRERELEASE version of %s. The most recent stable release is %d.%d.%d."
+                 name.Name
+                 version.Major
+                 version.Minor
+                 version.Build)
     try
-        let cmdLineParser = ArgumentParser.Create<CmdLine>(programName = name.Name, errorHandler = ProcessExiter())     
-        let parsed = cmdLineParser.Parse argv  
-        
-        if parsed.Contains Ops
-        then 
-            Seq.iter (fun (op : Operator) -> printfn "%s" <| op.Show()) checker.OperatorFactory.Operators
+        let cmdLineParser =
+            ArgumentParser.Create<CmdLine>(programName = name.Name, errorHandler = ProcessExiter())
+
+        let parsed = cmdLineParser.Parse argv
+        if parsed.Contains Ops then
+            Seq.iter (fun (op: Operator) -> printfn "%s" <| op.Show()) checker.OperatorFactory.Operators
             exit 0
         // if sequential
-        // then 
+        // then
         //     let proc = System.Diagnostics.Process.GetCurrentProcess()
-        //     proc.ProcessorAffinity <- nativeint 0x1          
+        //     proc.ProcessorAffinity <- nativeint 0x1        
+        let mutable jsonLog =
+            match parsed.TryGetResult json with
+            | None -> None
+            | Some _ ->
+                let log = ErrorMsg.Logger.LogToMemory()
+                { new Object() with 
+                    getLog () =  log }
+
         let run filename =
-            let interpreter = Interpreter(model,checker)
-            interpreter.Batch interpreter.DefaultLibDir filename    
-        match (parsed.TryGetResult Filename,ErrorMsg.isDebug()) with 
-            | None,false ->                                      
-                printfn "%s\n" (cmdLineParser.PrintUsage ())
-                0
-            | Some filename,_ -> 
-                run filename        
-                0
-            | None,true ->
-                run "test.imgql"
-                0
-    with e ->        
-            ErrorMsg.Logger.DebugExn e
-            ErrorMsg.Logger.Failure "exiting."
-            1
+            let interpreter = Interpreter(model, checker, )
+            interpreter.Batch interpreter.DefaultLibDir filename
+
+        match (parsed.TryGetResult Filename, ErrorMsg.isDebug ()) with
+        | None, false ->
+            printfn "%s\n" (cmdLineParser.PrintUsage())
+            0
+        | Some filename, _ ->
+            run filename
+            0
+        | None, true ->
+            run "test.imgql"
+            0
+    with e ->
+        ErrorMsg.Logger.DebugExn e
+        ErrorMsg.Logger.Failure "exiting."
+        1
