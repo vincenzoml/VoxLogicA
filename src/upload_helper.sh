fea7dad111bb4907bc94333503887288452d1265
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-02-06 18:49:53 +0100

    landing

--- src/upload_helper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-version=$(cat VERSION.txt)
-tag="v$version"
-files=../releases/VoxLogicA_$version_*.zip
-echo $files
-assets=$(printf -- "-a %s\n" $files)
-
-hub release create $assets $tag
