9fdb08f23076ce99a3d56861a36650d2413c1d91
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-03-05 22:39:48 +0100

    fixed upload helper

--- src/upload_helper.sh
+++ src/upload_helper.sh
@@ -4,7 +4,5 @@ version=$(cat VERSION.txt)
 tag="v$version"
 echo $version
 files=../releases/VoxLogicA_"$version"_*.zip
-echo $files
-assets=$(printf -- "-a %s\n" $files)
 
-gh release create -d $tag $assets
+gh release create $tag $files
