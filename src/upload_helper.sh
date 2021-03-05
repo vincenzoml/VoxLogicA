0fb20b0ee0622437e5d0d5465d0c91e16268954c
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-03-05 22:31:59 +0100

    release

--- src/upload_helper.sh
+++ src/upload_helper.sh
@@ -6,6 +6,4 @@ files=../releases/VoxLogicA_$version_*.zip
 echo $files
 assets=$(printf -- "-a %s\n" $files)
 
-echo gh release create $tag $assets
-
-echo please modify upload_helper.sh and replace "hub" with "gh release create v0.6.1-experimental VoxLogicA_0.6.1-experimental_*.zip -p --target experimental -t "experimental release with json output (--json command line option)" -n "please use the most recent stable release 0.6.0 instead"
+echo gh release create -d $tag $assets
