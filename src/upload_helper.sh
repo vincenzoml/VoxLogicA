978c827165d1141b4e276f3933aa77f0bd739135
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-04-25 09:53:41 +0200

    bumped version for release

--- src/upload_helper.sh
+++ src/upload_helper.sh
@@ -2,7 +2,8 @@
 
 version=$(cat VERSION.txt)
 tag="v$version"
+branch=$(git branch --show-current)
 echo $version
 files=../releases/PolyLogicA_"$version"_*.zip
 
-gh release create $tag $files
+gh release create $tag --target "$branch" $files
