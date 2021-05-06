96e13869c8227bf2521518f74a9b6f8297b3b787
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-05-06 16:53:44 +0200

    go

---
+++ src/viewer/lib/scripts/utils.js
@@ -0,0 +1,155 @@
+import * as THREE from './three.module.js';
+
+
+export function valDict(atoms) {
+    var dic = {}
+    atoms.forEach(atomName => {
+        dic[atomName] = [];
+    });
+    return dic;
+}
+
+export function udpateEval(evalDic, atomList) {
+    for (var atomName in evalDic) {
+        evalDic[atomName].push( atomList.includes(atomName) );
+    }
+}
+
+export function udpateEvalExt(evalDic, atomsData, i) {
+    for (var atomName in evalDic) {
+        evalDic[atomName].push( atomsData[atomName][i] );
+    }
+}
+
+export function determineEvalColor(arrayEvalColor, amount, simplex, params) {
+    if (simplex["atoms"].includes( params['propertyName'] )) {
+        for (let i = 0; i < amount; i++) { arrayEvalColor.push( new THREE.Color(params['truePropertyColor']) ); } 
+    } else {
+        for (let i = 0; i < amount; i++) { arrayEvalColor.push( new THREE.Color(params['falsePropertyColor']) ); } 
+    }
+}
+
+// export function addColorAttribute(objGeometry, objEvalColor) {
+//     const objColors = new Float32Array(objEvalColor.length * 4);
+//     for (let i = 0; i < objEvalColor.length; i++) {
+//         objColors[i*4  ] = objEvalColor[i]['r'];
+//         objColors[i*4+1] = objEvalColor[i]['g'];
+//         objColors[i*4+2] = objEvalColor[i]['b'];
+//         objColors[i*4+3] = 1;
+//     }
+//     // console.log(objColors);
+//     const objColorBuffer = new THREE.BufferAttribute(objColors, 4);
+//     objGeometry.setAttribute( 'color', objColorBuffer );
+// }
+
+
+export function addColorAttribute(bufferGeometry) {
+    const colorBuffer = new THREE.BufferAttribute(new Float32Array(bufferGeometry.getAttribute('position').count * 3), 3);
+    bufferGeometry.setAttribute( 'color', colorBuffer );
+}
+
+
+function hexToRGB(color) {
+    return [ Math.floor(color /65536)/255, Math.floor(color /256)%256/255, Math.floor(color %256)/255 ];
+}
+
+export function setColorBase(object, color) {
+    var colorRGB  = hexToRGB(color);
+    var colorBuffer = object.geometry.getAttribute('color');
+    for (let i = 0; i < colorBuffer.array.length; i++) {
+        colorBuffer.array[i] = colorRGB[i%3];
+    }
+    colorBuffer.needsUpdate = true;
+}
+
+export function setColorEval(object, valuation, verticesPerSimplex, trueColor, falseColor) {
+    // This function presupposes that geomtery has already a color attribute. See addColorAttribute.
+    var trueColorRGB  = hexToRGB(trueColor);
+    var falseColorRGB = hexToRGB(falseColor);
+    var colorBuffer = object.geometry.getAttribute('color');
+    for (let simplexIndex = 0; simplexIndex < valuation.length; simplexIndex++) {
+        for (let vertexIndex = 0; vertexIndex < verticesPerSimplex; vertexIndex++) {
+            colorBuffer.array[ (simplexIndex * verticesPerSimplex + vertexIndex) * 3   ] = valuation[simplexIndex] ? trueColorRGB[0] : falseColorRGB[0];
+            colorBuffer.array[ (simplexIndex * verticesPerSimplex + vertexIndex) * 3 +1] = valuation[simplexIndex] ? trueColorRGB[1] : falseColorRGB[1];
+            colorBuffer.array[ (simplexIndex * verticesPerSimplex + vertexIndex) * 3 +2] = valuation[simplexIndex] ? trueColorRGB[2] : falseColorRGB[2];
+        }
+    }
+    colorBuffer.needsUpdate = true;
+}
+
+
+// export function updateMaterialColor(material, useVertexColor, baseColor) {
+//     material.needsUpdate = true
+//     material.vertexColors = useVertexColor;
+//     if (useVertexColor) {
+//         material.color.set( 0xffffff);
+//     } else {
+//         material.color.set( baseColor );
+//     }
+// }
+
+
+
+export function computeResizedCoordinatesTetrahedron(tetrahedronResizedCoordinates, tetrahedronCoordinates, barycenter, sizeParameter) {
+    // Compute the resized coordinates
+    var smallerCoords = [
+        new THREE.Vector3().lerpVectors( barycenter, tetrahedronCoordinates[0], sizeParameter ),
+        new THREE.Vector3().lerpVectors( barycenter, tetrahedronCoordinates[1], sizeParameter ),
+        new THREE.Vector3().lerpVectors( barycenter, tetrahedronCoordinates[2], sizeParameter ),
+        new THREE.Vector3().lerpVectors( barycenter, tetrahedronCoordinates[3], sizeParameter )
+    ]
+
+    // Add the faces one at a time. Order matters!!!
+    tetrahedronResizedCoordinates.push( smallerCoords[0] );
+    tetrahedronResizedCoordinates.push( smallerCoords[1] );
+    tetrahedronResizedCoordinates.push( smallerCoords[2] );
+
+    tetrahedronResizedCoordinates.push( smallerCoords[0] );
+    tetrahedronResizedCoordinates.push( smallerCoords[3] );
+    tetrahedronResizedCoordinates.push( smallerCoords[1] );
+
+    tetrahedronResizedCoordinates.push( smallerCoords[1] );
+    tetrahedronResizedCoordinates.push( smallerCoords[3] );
+    tetrahedronResizedCoordinates.push( smallerCoords[2] );
+
+    tetrahedronResizedCoordinates.push( smallerCoords[0] );
+    tetrahedronResizedCoordinates.push( smallerCoords[2] );
+    tetrahedronResizedCoordinates.push( smallerCoords[3] );
+}
+
+export function resizeTetrahedrons (tetrahedronsGeometry, tetrahedronsCoordinates, tetrahedronsBarycenters, sizeParameter ) {
+    const positions = tetrahedronsGeometry.attributes.position.array;
+    const numberTetrahedrons = tetrahedronsBarycenters.length;
+    for (let i=0; i < numberTetrahedrons; ++i ) {
+        // console.log(positions);
+
+        var updatedCoords = [
+            new THREE.Vector3().lerpVectors( tetrahedronsBarycenters[i], tetrahedronsCoordinates[4*i  ], sizeParameter ),
+            new THREE.Vector3().lerpVectors( tetrahedronsBarycenters[i], tetrahedronsCoordinates[4*i+1], sizeParameter ),
+            new THREE.Vector3().lerpVectors( tetrahedronsBarycenters[i], tetrahedronsCoordinates[4*i+2], sizeParameter ),
+            new THREE.Vector3().lerpVectors( tetrahedronsBarycenters[i], tetrahedronsCoordinates[4*i+3], sizeParameter )
+        ]
+        // console.log(tetrahedronsCoordinates[4*i]);
+
+        // Same order as computeResizedCoordinatesTetrahedron function!!!
+        updatedCoords[0].toArray(positions, 36*i    );
+        updatedCoords[1].toArray(positions, 36*i + 3);
+        updatedCoords[2].toArray(positions, 36*i + 6);
+
+        updatedCoords[0].toArray(positions, 36*i + 9);
+        updatedCoords[3].toArray(positions, 36*i +12);
+        updatedCoords[1].toArray(positions, 36*i +15);
+
+        updatedCoords[1].toArray(positions, 36*i +18);
+        updatedCoords[3].toArray(positions, 36*i +21);
+        updatedCoords[2].toArray(positions, 36*i +24);
+
+        updatedCoords[0].toArray(positions, 36*i +27);
+        updatedCoords[2].toArray(positions, 36*i +30);
+        updatedCoords[3].toArray(positions, 36*i +33);
+
+    }
+}
+
+
+
