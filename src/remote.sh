ee8ae49507a4c0a7c566f17be644e3e4155bee2b
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-09-13 16:04:40 +0200

    WIP

--- src/remote.sh
+++ src/remote.sh
@@ -5,4 +5,4 @@ echo "repo: " $2
 echo "test: " $3
 echo "script: " $4
 
-ssh $1 "cd $2/src && git pull && make && cd tests/$3 && echo && echo && echo && echo RUNNING: && rm output/* && ../../bin/release/net5.0/linux-x64/VoxLogicA $4.imgql && echo && echo TEST: && cat $4.imgql && echo && echo && echo MD5: && md5sum output/*" && rm -f output/* && rsync -av pc-ciancia.isti.cnr.it:$2/src/tests/$3/output .
+ssh $1 "cd $2/src && git pull && make && cd tests/$3 && echo && echo && echo && echo RUNNING: && rm -f output/* && ../../bin/release/net5.0/linux-x64/VoxLogicA $4.imgql && echo && echo TEST: && cat $4.imgql && echo && echo && echo MD5: && md5sum output/*" && rm -f output/* && rsync -av pc-ciancia.isti.cnr.it:$2/src/tests/$3/output .
