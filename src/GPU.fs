4ac143d1daa928b548a87a41baa49880290c5c3c
laurab1 <l.bussi1@studenti.unipi.it>
2021-09-17 19:45:57 +0200

    wip

--- src/GPU.fs
+++ src/GPU.fs
@@ -303,7 +303,7 @@ and GPU(kernelsFilename : string, dimension : int) =
         
         let channelOrder =            
             match nComponents  with
-            | 1 -> CLEnum.R
+            | 1 -> CLEnum.Intensity
             | 4 -> CLEnum.Rgba
             | x -> raise <| UnsupportedNumberOfComponentsPerPixelException x
 
