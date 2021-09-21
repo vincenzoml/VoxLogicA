4ec8d81624987fa2087d093fb7f4a3caf6fad12f
laurab1 <l.bussi1@studenti.unipi.it>
2021-09-21 08:51:00 +0200

    WIP

--- src/tests/test-through/laura.sh
+++ src/tests/test-through/laura.sh
@@ -1,1 +1,1 @@
-git commit -am "WIP"; git push && rm -rf output && ../../remote.sh laurab@pc-ciancia.isti.cnr.it /home/laurab/VoxLogicA test-through test
+git commit -am "WIP"; git push && rm -rf output && ../../remote.sh laurab@pc-ciancia.isti.cnr.it /home/laurab/VoxLogicA test-through performance
