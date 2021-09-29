43c7a59f5ec91a6745f16689a82cf715a3196115
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-09-29 19:59:55 +0200

    improved bug test case

--- src/BUG/runme.sh
+++ src/BUG/runme.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 (cd .. && make)
+rm -rf output*
 ../bin/release/net5.0/linux-x64/VoxLogicA input.imgql
 mv output output-gpu
 /home/VoxLogicA/binaries/VoxLogicA_0.6.4.1-experimental_linux-x64/VoxLogicA input.imgql
