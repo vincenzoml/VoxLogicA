0f6ba8dbc38c819261b9072e6eb3106b35e761c2
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-10-11 12:48:09 +0200

    First release of VoxLogicA-GPU

--- src/BUG/runme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-(cd .. && make)
-rm -rf output*
-../bin/release/net5.0/linux-x64/VoxLogicA input.imgql
-mv output output-gpu
-../../../VoxLogicA_0.6.4.1-experimental_linux-x64/VoxLogicA input.imgql
-mv output output-cpu
-
-md5sum output*/*
