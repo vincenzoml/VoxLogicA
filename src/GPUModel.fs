12eceb8531a7dcbfa0ef3ac946addcec859186c2
laurab1 <l.bussi1@studenti.unipi.it>
2021-10-14 18:23:22 +0200

    testing

--- src/GPUModel.fs
+++ src/GPUModel.fs
@@ -309,6 +309,8 @@ type GPUModel() =
                         img1.Size,
                         None
                     )
+                gpu().Wait([|evt'|])
+                tmp.Get().Save("output/TMP.png")
                 let mutable newEvent = [|evt'|]
                 let iterations = int (ceil (Math.Log2(float img1.Size.[0])))
 
