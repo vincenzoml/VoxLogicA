acf5bdff8bd89a3c4f14741120aa9cd7207dd1b7
laurab1 <l.bussi1@studenti.unipi.it>
2021-10-15 10:29:23 +0200

    testing

--- src/GPUModel.fs
+++ src/GPUModel.fs
@@ -309,8 +309,8 @@ type GPUModel() =
                         img1.Size,
                         None
                     )
-                //gpu().Wait([|evt'|])
-                //tmp.Get().Save("output/TMP.nii.gz")
+                gpu().Wait([|evt'|])
+                tmp.Get().Save("output/TMP.nii.gz")
                 let mutable newEvent = [|evt'|]
                 let iterations = int (ceil (Math.Log2(float img1.Size.[0])))
 
@@ -334,8 +334,8 @@ type GPUModel() =
                                 None
                             )
 
-                    //gpu().Wait([|event|])
-                    //tmp.Get().Save(sprintf "output/iteration-%02d.nii.gz" i)
+                    gpu().Wait([|event|])
+                    tmp.Get().Save(sprintf "output/iteration-%02d.nii.gz" i)
 
                     newEvent <- [| event |]
                     swap ()
