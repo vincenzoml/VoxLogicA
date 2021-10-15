3ac734ccc01c80673b265b1d9e076fa9cdb87ffd
laurab1 <l.bussi1@studenti.unipi.it>
2021-10-15 10:52:16 +0200

    testing

--- src/GPUModel.fs
+++ src/GPUModel.fs
@@ -358,6 +358,7 @@ type GPUModel() =
                 )
                 gpu().Wait([|ev|])
                 let result = res.Get()
+                printf "%A" result
                 return float result.[0]
             }
 
