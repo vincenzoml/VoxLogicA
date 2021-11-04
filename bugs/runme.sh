4478d36bcf60a2350cfcd782641b130827a940c2
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-11-04 17:52:03 +0100

    MNWE

--- bugs/runme.sh
+++ bugs/runme.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
 
-(cd ../src && make)
+(cd ../src && make build-dbg)
 
 for m in cpu gpu; do
 fmla=x
 rm -rf in-$m.imgql out-$m
 cat << EOF >> in-$m.imgql
 load img="flair.nii.gz"
-let x=intensity(img) >. 100
-
-save "out-$m/x.nii.gz" x
+let x=intensity(img) >=. 100
 EOF
 
-for i in $(seq 1 10); do
+for i in $(seq 1 1); do
 
-cat << EOF >> in-$m.imgql
-save "out-$m/$i.nii.gz" $fmla
-EOF
+#cat << EOF >> in-$m.imgql
+#save "out-$m/$i.nii.gz" $fmla
+#EOF
 fmla="N $fmla"
 
 done
 
+cat << EOF >> in-$m.imgql
+save "out-$m/00_intensity.nii.gz" intensity(img)
+save "out-$m/01_thr.nii.gz" x
+save "out-$m/02_final.nii.gz" $fmla
+EOF
+
 done
 
-/home/VoxLogicA/binaries/VoxLogicA_0.6.4.3-experimental_linux-x64/VoxLogicA in-cpu.imgql
-../src/bin/release/net5.0/linux-x64/VoxLogicA in-gpu.imgql
+#/home/VoxLogicA/binaries/VoxLogicA_0.6.4.3-experimental_linux-x64/VoxLogicA in-cpu.imgql
+../src/bin/Debug/net5.0/linux-x64/VoxLogicA in-gpu.imgql
 
-diff -qr out-{cpu,gpu}
-echo diff: $?
+#diff -qr out-{cpu,gpu}
+#echo diff: $?
+#echo ---
+#md5sum out-cpu/*
+#echo ---
+md5sum out-gpu/*
+echo ---
