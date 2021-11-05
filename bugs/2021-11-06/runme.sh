2b7fe5a5cd6bf02b3bda7a20875315fa601ad5f7
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-11-05 13:04:49 +0100

    WIP

--- bugs/2021-11-06/runme.sh
+++ bugs/2021-11-06/runme.sh
@@ -1,10 +1,12 @@
+(cd ../../src && make build-dbg)
+
 
 rm -rf output* input.imgql
 
 /home/VoxLogicA/scripts/glue_analysis.sh /home/VoxLogicA/datasets/BraTS_2019_HGG_Augmented#2Cases glue.imgql  ./input.imgql output
 /home/VoxLogicA/binaries/VoxLogicA_0.6.4.3-experimental_linux-x64/VoxLogicA input.imgql
 mv output output-cpu
-../src/bin/release/net5.0/linux-x64/VoxLogicA input.imgql
+../../src/bin/Debug/net5.0/linux-x64/VoxLogicA input.imgql
 mv output output-gpu
 
 echo ---
