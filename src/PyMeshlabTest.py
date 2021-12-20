d60b30de3d5b61e9d5c77368b9750b989771256b
Vincenzo Ciancia <vincenzoml@gmail.com>
2021-12-20 11:28:02 +0100

    perhaps the select_connected_faces filter in pymeshlab does not work as expected

--- src/PyMeshlabTest.py
+++ src/PyMeshlabTest.py
@@ -1,12 +1,9 @@
 # %%
-
 import pymeshlab
 ms = pymeshlab.MeshSet()
-ms.load_new_mesh('data/local/repos/VoxLogicA/src/RAJA2-simplified.obj')
-# %%
-ms.select_faces_by_color(color=pymeshlab.Color(255,0,0,255),colorspace='RGB')
-#ms.select_connected_faces()
-# %%
-ms.vertex_color_filling(color1=pymeshlab.Color(0,255,255,127),onselected=True)
-# %%
+ms.load_new_mesh('RAJA2-simplified.obj')
+ms.select_faces_by_color(color=pymeshlab.Color(255,0,255),colorspace='RGB')
+ms.select_connected_faces()
+ms.vertex_color_filling(color1=pymeshlab.Color(0,0,0),onselected=True)
 ms.save_current_mesh("out.obj")
+# %%
