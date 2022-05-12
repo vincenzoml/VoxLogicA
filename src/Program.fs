d58c267ef375204314ea699118fa67b5dd91374f
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-05-13 00:00:59 +0200

    STILL WORKING

--- src/Program.fs
+++ src/Program.fs
@@ -1,14 +1,15 @@
 // SYNTAX
-type Expression = 
+type Expression =
     | ECall of string * (Expression list)
-    | ENumber of float    
-type Command = 
-    | Declaration of string * (string list) * Expression    
+    | ENumber of float
+
+type Command =
+    | Declaration of string * (string list) * Expression
     | Print of Expression
-    
+
 type Program = Program of list<Command>
-    
-// SEMANTICS 
+
+// SEMANTICS
 type Eval = float
 
 type DVal =
@@ -32,6 +33,12 @@ and Environment =
                  | Environment env -> env)
         )
 
+    member this.TryFind ide =
+        Map.tryFind
+            ide
+            (match this with
+             | Environment env -> env)
+
     member this.BindList idelist exprlist =
         match (idelist, exprlist) with
         | (ide :: ides, expr :: exprs) -> (this.Bind ide expr).BindList ides exprs
@@ -40,57 +47,62 @@ and Environment =
 
 let emptyEnvironment = Environment Map.empty
 
-let rec reduceProgramRec<'a> (env : Environment,prints : List<Eval>) (Program p) (cont : List<Eval> -> 'a) =
+let rec reduceProgramRec<'a> (env: Environment, prints: List<Eval>) (Program p) (cont: List<Eval> -> 'a) =
     match p with
     | [] -> cont (List.rev prints)
     | command :: commands ->
-        reduceCommand (env, prints) command <|
-            fun (env',tasks') -> 
-                reduceProgramRec (env',tasks') (Program commands) cont 
+        reduceCommand (env, prints) command
+        <| fun (env', tasks') -> reduceProgramRec (env', tasks') (Program commands) cont
 
 and reduceCommand (env, prints) command cont =
     match command with
-    | Declaration (ide, formalArgs, body) -> 
-        cont (env.Bind ide (Fun(env, formalArgs, body)), prints)
+    | Declaration (ide, formalArgs, body) -> cont (env.Bind ide (Fun(env, formalArgs, body)), prints)
     | Print expr -> // TODO: use pos
-        reduceExpr env expr <|
-            fun result ->
-                cont (env, result::prints)
+        reduceExpr env expr
+        <| fun result -> cont (env, result :: prints)
 
-and reduceExpr (env: Environment) (expr : Expression) (cont : Eval -> 'a) =
+and reduceExpr (env: Environment) (expr: Expression) (cont: Eval -> 'a) =
     match expr with
     | ENumber f -> cont f
     | ECall (ide, args) -> // TODO: use pos
         let rec reduceArgs args accum cont =
-            match args with 
+            match args with
             | [] -> cont (List.rev accum)
-            | arg::args' -> 
-                reduceExpr env arg <|
-                    fun eval ->
-                        reduceArgs args' (eval::accum) cont
+            | arg :: args' ->
+                reduceExpr env arg
+                <| fun eval -> reduceArgs args' (eval :: accum) cont
 
-        reduceArgs args [] <|
-            fun actualArgs ->
-                match env.Find ide with
-                    | Fun (denv, formalArgs, body) ->
-                        let callEnv =
-                            if formalArgs.Length = args.Length then
-                                denv.BindList formalArgs (List.map Result actualArgs)
-                            else
-                                failwith (sprintf "%s requires %d arguments, got %d" ide formalArgs.Length args.Length)                                    
+        reduceArgs args []
+        <| fun actualArgs ->
+            match env.TryFind ide with
+            | Some (Fun (denv, formalArgs, body)) ->
+                let callEnv =
+                    if formalArgs.Length = args.Length then
+                        denv.BindList formalArgs (List.map Result actualArgs)
+                    else
+                        failwith (sprintf "%s requires %d arguments, got %d" ide formalArgs.Length args.Length)
 
-                        reduceExpr callEnv body <| cont                            
-                    | Result r -> 
-                        cont r                
+                reduceExpr callEnv body <| cont
+            | Some (Result r) -> cont r
+            | None -> failwith $"unbound identifier {ide}"
 
 let reduceProgram prog =
     reduceProgramRec (emptyEnvironment, []) prog id
 
-let prog n = 
-    let declarations = (Declaration ("t0",["x"],ECall ("x",[])))::(List.init n (fun i -> Declaration ($"t{i+1}",["x"],ECall ($"t{i}",[ECall ("x",[])]) )))
-    (List.append declarations [Print (ECall ($"t{n}",[ENumber 0]))])
+let prog n =
+    let declarations =
+        (Declaration("t0", [ "x" ], ECall("x", [])))
+        :: (List.init n (fun i -> Declaration($"t{i + 1}", [ "x" ], ECall($"t{i}", [ ECall("x", []) ]))))
 
-let p = prog <| try (int (System.Environment.GetCommandLineArgs()[1])) with _ -> 1000000
+    (List.append declarations [ Print(ECall($"t{n}", [ ENumber 0 ])) ])
+
+let p =
+    prog
+    <| try
+        (int (System.Environment.GetCommandLineArgs()[1]))
+       with
+       | _ -> 1000000
+
 printfn $"Program length: {p.Length}"
 let redux = reduceProgram (Program p)
 printfn $"Redux length: {redux.Length}"
