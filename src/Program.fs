2a2337830f9b02cb587e756ec002df0b8f426a82
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-05-12 23:56:08 +0200

    WIP

--- src/Program.fs
+++ src/Program.fs
@@ -1,98 +1,76 @@
 // SYNTAX
-type Expression = 
+type Expression =
     | ECall of string * (Expression list)
-    | ENumber of float    
-type Command = 
-    | Declaration of string * (string list) * Expression    
-    | Print of Expression
+type Command =
+    | Declaration of string * (string list) * Expression
     
-type Program = Program of list<Command>
-    
-// SEMANTICS 
-type Eval = float
+type Program = Program of (list<Command> * Expression)
 
 type DVal =
-    | Result of Eval
+    | Result of unit
     | Fun of Environment * List<string> * Expression
 
-and Environment =
-    | Environment of Map<string, DVal>
-    member this.Find ide =
-        Map.find
-            ide
-            (match this with
-             | Environment env -> env)
+and Environment = Map<string, DVal>
 
-    member this.Bind ide expr =
-        Environment(
-            Map.add
-                ide
-                expr
-                (match this with
-                 | Environment env -> env)
-        )
-
-    member this.BindList idelist exprlist =
+let rec bindList env idelist exprlist = 
         match (idelist, exprlist) with
-        | (ide :: ides, expr :: exprs) -> (this.Bind ide expr).BindList ides exprs
-        | ([], []) -> this
+        | (ide :: ides, expr :: exprs) -> bindList (Map.add ide expr env) ides exprs
+        | ([], []) -> env
         | _ -> failwith "Internal error in module Reducer. Please report."
 
-let emptyEnvironment = Environment Map.empty
-
-let rec reduceProgramRec<'a> (env : Environment,prints : List<Eval>) (Program p) (cont : List<Eval> -> 'a) =
+let rec reduceProgramRec (env: Environment) (Program (p,ret)) cont =
     match p with
-    | [] -> cont (List.rev prints)
+    | [] -> reduceExpr env ret cont
     | command :: commands ->
-        reduceCommand (env, prints) command <|
-            fun (env',tasks') -> 
-                reduceProgramRec (env',tasks') (Program commands) cont 
+        reduceCommand env command
+        <| fun env' -> reduceProgramRec env' (Program (commands,ret)) cont
 
-and reduceCommand (env, prints) command cont =
+and reduceCommand env command cont =
     match command with
-    | Declaration (ide, formalArgs, body) -> 
-        cont (env.Bind ide (Fun(env, formalArgs, body)), prints)
-    | Print expr -> // TODO: use pos
-        reduceExpr env expr <|
-            fun result ->
-                cont (env, result::prints)
+    | Declaration (ide, formalArgs, body) -> cont (Map.add ide (Fun(env, formalArgs, body)) env)
 
-and reduceExpr (env: Environment) (expr : Expression) (cont : Eval -> 'a) =
+and reduceExpr (env: Environment) (expr: Expression) (cont: unit -> unit) =
     match expr with
-    | ENumber f -> cont f
+    | ECall (_,[]) -> ()
     | ECall (ide, args) -> // TODO: use pos
         let rec reduceArgs args accum cont =
-            match args with 
+            match args with
             | [] -> cont (List.rev accum)
-            | arg::args' -> 
-                reduceExpr env arg <|
-                    fun eval ->
-                        reduceArgs args' (eval::accum) cont
+            | arg :: args' ->
+                reduceExpr env arg
+                <| fun eval -> reduceArgs args' (eval :: accum) cont
 
-        reduceArgs args [] <|
-            fun actualArgs ->
-                match env.Find ide with
-                    | Fun (denv, formalArgs, body) ->
-                        let callEnv =
-                            if formalArgs.Length = args.Length then
-                                denv.BindList formalArgs (List.map Result actualArgs)
-                            else
-                                failwith (sprintf "%s requires %d arguments, got %d" ide formalArgs.Length args.Length)                                    
+        reduceArgs args []
+        <| fun actualArgs ->
+            match (Map.find ide env) with
+            | (Fun (denv, formalArgs, body)) ->
+                let callEnv =
+                    if formalArgs.Length = args.Length then
+                        bindList denv formalArgs (List.map Result actualArgs)
+                    else
+                        failwith (sprintf "%s requires %d arguments, got %d" ide formalArgs.Length args.Length)
 
-                        reduceExpr callEnv body <| cont                            
-                    | Result r -> 
-                        cont r                
+                reduceExpr callEnv body <| cont
+            | (Result r) -> cont r            
 
 let reduceProgram prog =
-    reduceProgramRec (emptyEnvironment, []) prog id
+    reduceProgramRec Map.empty prog id
 
-let prog n = 
-    let declarations = (Declaration ("t0",["x"],ECall ("x",[])))::(List.init n (fun i -> Declaration ($"t{i+1}",["x"],ECall ($"t{i}",[ECall ("x",[])]) )))
-    (List.append declarations [Print (ECall ($"t{n}",[ENumber 0]))])
+let prog n =
+    let declarations =
+        (Declaration("t0", [ "x" ], ECall("x", [])))
+        :: (List.init n (fun i -> Declaration($"t{i + 1}", [ "x" ], ECall($"t{i}", [ ECall("x", []) ]))))
 
-let p = prog <| try (int (System.Environment.GetCommandLineArgs()[1])) with _ -> 1000000
-printfn $"Program length: {p.Length}"
-let redux = reduceProgram (Program p)
-printfn $"Redux length: {redux.Length}"
+    (declarations, ECall($"t{n}", [ ECall ("finish",[]) ]))
 
-// To run: dotnet run -c release 100000 (stack overflow); smaller numbers will works (try 10)
+let (decl,ret) =
+    prog
+    <| try
+        (int (System.Environment.GetCommandLineArgs()[1]))
+       with
+       | _ -> 1000000
+
+printfn $"Program length: {decl.Length}"
+let redux = reduceProgram (Program (decl,ret))
+printfn $"{redux}"
+
