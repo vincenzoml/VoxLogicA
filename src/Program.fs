30eb5dae4744b174ab810cf3660e19e53264c924
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-06-22 12:43:16 +0200

    WIP

--- src/Program.fs
+++ src/Program.fs
@@ -29,7 +29,7 @@ type LoadFlags = {
 type CmdLine = 
     | Ops 
     | Sequential
-    | Convert of string * string
+    | Convert of string * string    
     | [<MainCommandAttribute;UniqueAttribute>] Filename of string    
 with
     interface Argu.IArgParserTemplate with
@@ -37,8 +37,23 @@ with
             match s with
             | Ops ->  "display a list of all the internal operators, with their types and a brief description"
             | Filename _ -> "VoxLogicA session file"
-            | Convert _ ->  "Convert from an image to a json"
+            | Convert _ ->  "Convert from/to image, json, aut (mcrl2)"
             | Sequential ->  "Run on one CPU only"
+
+let (|Img|_|) (s : string) =
+    match System.IO.Path.GetExtension s with
+    | (".png"|".jpg"|".bmp"|".nii.gz"|".nii") -> Some Img
+    | _ -> None
+
+let (|JSon|_|) (s : string) =
+    match System.IO.Path.GetExtension s with
+    | ".json" -> Some JSon
+    | _ -> None
+
+let (|Aut|_|) (s: string) =
+    match System.IO.Path.GetExtension s with
+    | ".aut" -> Some Aut
+    | _ -> None
     
 [<EntryPoint>]
 let main (argv : string array) =
@@ -58,88 +73,104 @@ let main (argv : string array) =
             Seq.iter (fun (op : Operator) -> printfn "%s" <| op.Show()) checker.OperatorFactory.Operators
             exit 0
         if parsed.Contains Convert then          
-            match parsed.GetResult Convert with
-            | (s1,s2) ->
-                match (System.IO.Path.GetExtension s1,System.IO.Path.GetExtension s2) with                
-                | _,".json" ->
-                    let (imgf,jsonf) = (s1,s2)
-                    let img = new SITKUtil.VoxImage(imgf)                  
-                    if img.NComponents < 3 then failwith "Image must be RGB or RGBA"
-                    let s = 
-                        let t = img.Size
-                        if Array.length t = 3 then t 
-                        else Array.append t [| 1 |]
-                    img.GetBufferAsFloat (fun buf ->
-                        System.IO.File.WriteAllText(jsonf,FSharp.Json.Json.serialize {  
-                            Graph.nodes = List.ofSeq <| seq { 
-                                for i in 0..(s.[0]-1) do 
-                                for j in 0..(s.[1]-1) do 
-                                for k in 0..(s.[2]-1) do { 
-                                    Graph.id = string (i,j,k);                                
-                                    Graph.atoms = [ 
-                                        let start = img.NComponents * (i + (j*s.[0]) + (k*s.[1]))
-                                        let r = buf.Get start
-                                        let g = buf.Get (start+1)
-                                        let b = buf.Get (start+2)
-                                        sprintf "#%02X%02X%02X" (int r) (int g) (int b)
-                                        ] 
-                                } 
-                            };
-                            Graph.arcs = List.ofSeq <| seq { 
-                                for i in 0..(s.[0]-1) do 
-                                for j in 0..(s.[1]-1) do 
-                                for k in 0..(s.[2]-1) do                            
-                                for a in -1..1 do
-                                for b in -1..1 do
-                                for c in -1..1 do
-                                let d = i+a
-                                let e = j+b
-                                let f = k+c
-                                // if 0 <= d && d < s.[0] && 0 <= e && e < s.[1] && 0 <= f && f < s.[2] then { 
-                                if (List.length (List.filter (fun x -> x <> 0) [a;b;c])) = 1 && 0 <= d && d < s.[0] && 0 <= e && e < s.[1] && 0 <= f && f < s.[2] then { 
-                                    Graph.source = string (i,j,k); 
-                                    Graph.target = string (d,e,f)
-                                }
+            let s1,s2 = parsed.GetResult Convert
+            match s1,s2 with                
+            | Img,JSon ->
+                let (imgf,jsonf) = (s1,s2)
+                let img = new SITKUtil.VoxImage(imgf)                  
+                if img.NComponents < 3 then failwith "Image must be RGB or RGBA"
+                let s = 
+                    let t = img.Size
+                    if Array.length t = 3 then t 
+                    else Array.append t [| 1 |]
+                img.GetBufferAsFloat (fun buf ->
+                    System.IO.File.WriteAllText(jsonf,FSharp.Json.Json.serialize {  
+                        Graph.nodes = List.ofSeq <| seq { 
+                            for i in 0..(s.[0]-1) do 
+                            for j in 0..(s.[1]-1) do 
+                            for k in 0..(s.[2]-1) do { 
+                                Graph.id = string (i,j,k);                                
+                                Graph.atoms = [ 
+                                    let start = img.NComponents * (i + (j*s.[0]) + (k*s.[1]))
+                                    let r = buf.Get start
+                                    let g = buf.Get (start+1)
+                                    let b = buf.Get (start+2)
+                                    sprintf "#%02X%02X%02X" (int r) (int g) (int b)
+                                    ] 
+                            } 
+                        };
+                        Graph.arcs = List.ofSeq <| seq { 
+                            for i in 0..(s.[0]-1) do 
+                            for j in 0..(s.[1]-1) do 
+                            for k in 0..(s.[2]-1) do                            
+                            for a in -1..1 do
+                            for b in -1..1 do
+                            for c in -1..1 do
+                            let d = i+a
+                            let e = j+b
+                            let f = k+c
+                            // if 0 <= d && d < s.[0] && 0 <= e && e < s.[1] && 0 <= f && f < s.[2] then { 
+                            if (List.length (List.filter (fun x -> x <> 0) [a;b;c])) = 1 && 0 <= d && d < s.[0] && 0 <= e && e < s.[1] && 0 <= f && f < s.[2] then { 
+                                Graph.source = string (i,j,k); 
+                                Graph.target = string (d,e,f)
                             }
-                        })                 
-                    )
-                | ".json",_ ->
-                    let j = Graph.loadFileGraph(s1)
-                    let parseTriple s = 
-                        let reg = new System.Text.RegularExpressions.Regex "^\s*\((\s*[0-9]+\s*),(\s*[0-9]+\s*),(\s*[0-9]+\s*)\)\s*$"
-                        let m = reg.Match s
-                        if m.Success 
-                        then [|int m.Groups[1].Value;int m.Groups[2].Value;int m.Groups[3].Value|]
-                        else failwith $"Not a triple: {s}"
+                        }
+                    })                 
+                )
+            | JSon,Img ->
+                let j = Graph.loadFileGraph(s1)
+                let parseTriple s = 
+                    let reg = new System.Text.RegularExpressions.Regex "^\s*\((\s*[0-9]+\s*),(\s*[0-9]+\s*),(\s*[0-9]+\s*)\)\s*$"
+                    let m = reg.Match s
+                    if m.Success 
+                    then [|int m.Groups[1].Value;int m.Groups[2].Value;int m.Groups[3].Value|]
+                    else failwith $"Not a triple: {s}"
 
-                    let parseColour s =
-                        let reg = new System.Text.RegularExpressions.Regex "^\s*#([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])\s*$"
-                        let m = reg.Match s
-                        let f x = System.Int32.Parse(x, System.Globalization.NumberStyles.HexNumber)
-                        if m.Success
-                        then [|f m.Groups[1].Value;f m.Groups[2].Value;f m.Groups[3].Value|]
-                        else failwith $"Not a colour: {s}"
-                    
-                    let nodes = 
-                        seq { for n in j.nodes do
-                                yield (parseTriple n.id,parseColour n.atoms[0]) }
-                    let maxdim i = 
-                        let fn (x : array<int> * array<int>) =                             
-                            let coords = fst x
-                            coords[i]
-                        (Seq.max (Seq.map fn nodes))                        
-                    let imgDim = [|maxdim 0;maxdim 1;maxdim 2|]
-                    let img = new SITKUtil.VoxImage(new itk.simple.Image(new itk.simple.VectorUInt32(Array.map uint32 imgDim),itk.simple.PixelIDValueEnum.sitkVectorUInt8,uint32 4))
-                    img.GetBufferAsUInt8 (fun buf ->
-                        for (coords,colour) in nodes do
-                            let linearCoord = coords[0] + (coords[1] * imgDim[0]) + (coords[2] * imgDim[1] * imgDim[0])
-                            for colourId in [0..2] do
-                                buf.Set (linearCoord + colourId) (uint8 colour[colourId])
-                            buf.Set (linearCoord + 3) 255uy
-                    )
-                    img.Save s2
-                    
-                | _,_ ->
+                let parseColour s =
+                    let reg = new System.Text.RegularExpressions.Regex "^\s*#([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])([0-9a-fA-F][0-9a-fA-F])\s*$"
+                    let m = reg.Match s
+                    let f x = System.Int32.Parse(x, System.Globalization.NumberStyles.HexNumber)
+                    if m.Success
+                    then [|f m.Groups[1].Value;f m.Groups[2].Value;f m.Groups[3].Value|]
+                    else failwith $"Not a colour: {s}"
+                
+                let nodes = 
+                    seq { for n in j.nodes do
+                            yield (parseTriple n.id,parseColour n.atoms[0]) }
+                let maxdim i = 
+                    let fn (x : array<int> * array<int>) =                             
+                        let coords = fst x
+                        coords[i]
+                    (Seq.max (Seq.map fn nodes))                        
+                let imgDim = [|maxdim 0;maxdim 1;maxdim 2|]
+                let img = new SITKUtil.VoxImage(new itk.simple.Image(new itk.simple.VectorUInt32(Array.map uint32 imgDim),itk.simple.PixelIDValueEnum.sitkVectorUInt8,uint32 4))
+                img.GetBufferAsUInt8 (fun buf ->
+                    for (coords,colour) in nodes do
+                        let linearCoord = coords[0] + (coords[1] * imgDim[0]) + (coords[2] * imgDim[1] * imgDim[0])
+                        for colourId in [0..2] do
+                            buf.Set (linearCoord + colourId) (uint8 colour[colourId])
+                        buf.Set (linearCoord + 3) 255uy
+                )
+                img.Save s2
+            | JSon,Aut ->
+                let j = Graph.loadFileGraph(s1)
+                let nodeOfId = new System.Collections.Generic.Dictionary<_,_>()
+                List.iteri (fun i (v : Graph.IntNode) -> nodeOfId[v.id] <- {| index = i; node = v|}) j.nodes
+                let transitions = new System.Collections.Generic.HashSet<_>()                                
+                for a in j.arcs do
+                    ignore <| transitions.Add (nodeOfId[a.source].index,"tau",nodeOfId[a.target].index)
+                for kv in nodeOfId do
+                    for atom in kv.Value.node.atoms do
+                        ignore <| transitions.Add (kv.Value.index,atom,kv.Value.index)
+                printfn "des (0,%A,%A)" transitions.Count j.nodes.Length
+                for t in transitions do                    
+                    printfn "%A" t                                        
+                exit 0
+
+
+
+                
+            | _,_ ->
                     failwith "wrong file exensions for conversion"
             ErrorMsg.Logger.Debug "Conversion done."
             exit 0
