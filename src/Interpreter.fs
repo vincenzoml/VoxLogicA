33f2373783bc91e5b2393e6aee8da9cff2065741
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-07-20 17:17:33 +0200

    WIP -- BROKEN

--- src/Interpreter.fs
+++ src/Interpreter.fs
@@ -16,48 +16,61 @@ http://moiraesoftware.github.io/blog/2012/01/22/FSharp-Dataflow-agents-I/
 *)
 
 type ResourceType = Unit
-type Resource = { resourceType: ResourceType }
 
-type Constraint =
-    | Exact of int
-    static member combine c1 c2 =
-        match c1, c2 with
-        | (Exact i1, Exact i2) -> Exact(i1 + i2)
+type ResourceKey = string
 
-type Requirements(dict: Dictionary<ResourceType, Constraint>) =
-    new(s: seq<ResourceType * Constraint>) =
-        let dict = new Dictionary<_, _>()
+type Requirements = IDictionary<ResourceKey,ResourceType>
 
-        Seq.iter
-            (fun (resourceType, a) ->
-                dict[resourceType] <- if dict.ContainsKey resourceType then
-                                          Constraint.combine dict[resourceType] a
-                                      else
-                                          a)
-            s
+type Resource(resourceType) =
+    let mutable assignedTo = None
+    member val ResourceType: ResourceType = resourceType
+    member __.AssignedTo = assignedTo
+    member __.AssignTo rs =
+        assert assignedTo.IsNone
+        assignedTo <- Some rs
+    member __.Reclaim() =
+        assert assignedTo.IsSome
+        assignedTo <- None
+and Resources() =
+    let byId = new Dictionary<ResourceKey,Resource>()
+    let byType = new Dictionary<ResourceType,list<Resource>>()
 
-        Requirements(dict)
+    member __.ById id = byId[id]
+    member __.ByType t = byType[t] :> seq<Resource>
 
-    member __.ResourceTypes = dict.Keys
-    member __.Item x = dict[x]
+    member this.Assign(id,resource : Resource) =
+        assert not (byId.ContainsKey id)
+        byId[id] <- resource
+        let t = resource.ResourceType
+        byType[t] <- resource::byType[t]
+        resource.AssignTo this
+
+    member __.Reclaim() = // deliberately not fine-grained
+        let v = byId.Values
+        for res in v do
+            res.Reclaim()
+        byId.Clear()
+        byType.Clear()
+        v
 
 
-type Resources(dict: Dictionary<ResourceType, Set<Resource>>) =
-    new(s: seq<ResourceType * Set<Resource>>) =
-        let dict = new Dictionary<_, _>()
 
-        Seq.iter
-            (fun (resourceType, a) ->
-                dict[resourceType] <- if dict.ContainsKey resourceType then
-                                          Set.union dict[resourceType] a
-                                      else
-                                          a)
-            s
 
-        Resources(dict)
 
-    member __.ResourceTypes = dict.Keys
-    member __.Item x = dict[x]
+    // new(s: seq<ResourceType * Set<Resource>>) =
+    //     new ResourceData(s,Set.union)
+    // //     let dict = new Dictionary<_, _>()
+
+    //     Seq.iter
+    //         (fun (resourceType, a) ->
+    //             dict[resourceType] <- if dict.ContainsKey resourceType then
+    //                                       Set.union dict[resourceType] a
+    //                                   else
+    //                                       a)
+    //         s
+
+    //     Resources(dict)
+
 
 [<AbstractClass>]
 type OperatorImplementation<'t>(requirements: Requirements) =
@@ -68,19 +81,17 @@ type OperatorImplementation<'t>(requirements: Requirements) =
 type ExecutionEngine<'t>() =
     abstract member ImplementationOf: Operator -> OperatorImplementation<'t>
 
-[<AbstractClass>]
-type ComputeUnit<'t>(executionEngine: ExecutionEngine<'t>, taskId, task) =
-    let operatorImplementation = executionEngine.ImplementationOf task.operator
 
-    member __.TaskId = taskId
-    member __.Task = task
+[<AbstractClass>]
+type ComputeUnit<'t>(executionEngine: ExecutionEngine<'t>, operator, arguments) =
+    let operatorImplementation = executionEngine.ImplementationOf operator
 
     member val Requirements = operatorImplementation.Requires
 
-// member val Resources = Resources []
+    member val Resources = Resources()
 
-// member ... Run ...
+    member __.Run()
 
-let mkComputeUnit id (task: Task) = failwith "stub"
+// let mkComputeUnit id (task: Task) = failwith "stub"
 
-let runTaskGraph (program: WorkPlan) = Array.mapi mkComputeUnit program.tasks
+// let runTaskGraph (program: WorkPlan) = Array.mapi mkComputeUnit program.tasks
