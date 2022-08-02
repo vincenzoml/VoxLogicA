9af8a75c19ec6cab78acd7ca7adca5d248870874
Vincenzo Ciancia <vincenzoml@gmail.com>
2022-08-02 15:53:16 +0200

    really works

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import random
 
 
-for i in range(0,30):
-    k = random.randint(34,42)
+for i in range(0,10000):
+    k = random.randint(38,41)
 
     print(f'''print "fib({k},{i})" fib({k},{i})''')
