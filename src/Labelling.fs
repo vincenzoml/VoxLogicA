8f8635a953021af335f171cb76e6deea928735d0
Vincenzo Ciancia <vincenzoml@gmail.com>
2024-09-14 08:31:56 +0000

    Updated README.md and commented the file Labelling.fs

--- src/Labelling.fs
+++ src/Labelling.fs
@@ -8,6 +8,8 @@ open Reducer
 // For now, it only returns a copy of the array of DAG nodes, in unspecified
 // order, just as they come from the Reducer module.
 
+// Check the type WorkPlan and associated types.
+
 let label (workplan : WorkPlan) =
     Array.copy workplan.operations
 
